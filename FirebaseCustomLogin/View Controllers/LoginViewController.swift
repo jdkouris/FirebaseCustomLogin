57c77511c8c37fbc1d732052c9cb263d3c43aaff
John Kouris <jdkouris@gmail.com>
2019-10-10 16:01:41 -0700

    Wired up the outlets and actions

--- FirebaseCustomLogin/View Controllers/LoginViewController.swift
+++ FirebaseCustomLogin/View Controllers/LoginViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +21,10 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+    }
+    
     /*
     // MARK: - Navigation
 
--- FirebaseCustomLogin/View Controllers/SignUpViewController.swift
+++ FirebaseCustomLogin/View Controllers/SignUpViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
-    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +23,10 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        
+    }
+    
     /*
     // MARK: - Navigation
 
